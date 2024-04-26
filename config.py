4155661abe67f8538f3bf640caee7571a6de7add
christophermccall <camccall24@gmail.com>
2024-04-26 10:31:54 -0400

    changed config

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ basedir = path.abspath(path.dirname(__file__))
 class Config:
     """Set Flask configuration from .env file."""
     # General Config
-    SECRET_KEY = f'{input('secret key')}'
+    SECRET_KEY = f'{input("secret key")}'
     FLASK_APP = 'circle.app'
     # Database
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///drumcircle.db'
-    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://chrism:{input('db password')}@localhost/drumcircle'
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://chrism:{input("db password")}@localhost/drumcircle'
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
