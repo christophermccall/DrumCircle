d803680575265980c8ddb9eb96dc8cfd39a66be8
Christopher McCall <camccall24@gmail.com>
2024-09-15 19:11:58 -0400

    fixed bug in connection to mysql

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config:
     # Database
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///drumcircle.db'
     # SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://chrism:{password}@localhost/drumcircle'
-    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{user}@{host}:{port}/{database}?charset=utf8mb4'
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset=utf8mb4'
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # commit test 2
